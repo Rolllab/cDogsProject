eab4a330b95ad419635df2cf63b0ac6e44bd8455
Дмитрий Волков <roskey@mail.ru>
2025-04-08 19:24:17 +0300

    Добавлены настройки для CBV users

--- config/settings.py
+++ config/settings.py
@@ -153,7 +153,6 @@ MEDIA_ROOT = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
-
 LOGIN_REDIRECT_URL = 'dogs:index'
 LOGOUT_REDIRECT_URL = 'dogs:index'
 LOGIN_URL = '/users/'
