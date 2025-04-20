17b08710acf34b072bbee553b47885c460100d52
Дмитрий Волков <roskey@mail.ru>
2025-04-20 15:55:15 +0300

    Исправлена ошибка формы

--- dogs/forms.py
+++ dogs/forms.py
@@ -12,9 +12,10 @@ class DogForm(StyleFormMixin, forms.ModelForm):
 
     def clean_birth_date(self):
         cleaned_data = self.cleaned_data['birth_date']
-        now_year = datetime.now().year
-        if now_year - cleaned_data.year > 35:
-            raise forms.ValidationError('Собака должна быть моложе 35 лет')
+        if cleaned_data:
+            now_year = datetime.now().year
+            if now_year - cleaned_data.year > 35:
+                raise forms.ValidationError('Собака должна быть моложе 35 лет')
         return cleaned_data
 
 
