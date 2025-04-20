0b5368373be55648339271cb38370f62a01691f0
Дмитрий Волков <roskey@mail.ru>
2025-04-20 20:25:20 +0300

    1) Добавлена карточка для показа пользователей; 2) Реализация html страницы для показа пользователей; 3) Добавлены соответствующие urls; 4) Создана соответствующая view; 5) Конец урока 19-20.

--- users/urls.py
+++ users/urls.py
@@ -2,16 +2,20 @@ from django.urls import path
 
 from users.apps import UsersConfig
 from users.views import UserRegisterView, UserLoginView, UserProfileView, UserLogoutView, UserUpdateView, \
-    UserPasswordChangeView, user_generate_new_password_view
+    UserPasswordChangeView, user_generate_new_password_view, UserListView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    # Работа с аккаунтом
     path('', UserLoginView.as_view(), name='user_login'),
     path('logout/', UserLogoutView.as_view(), name='user_logout'),
     path('register/', UserRegisterView.as_view(), name='user_register'),
     path('profile/', UserProfileView.as_view(), name='user_profile'),
     path('update/', UserUpdateView.as_view(), name='user_update'),
     path('change_password/', UserPasswordChangeView.as_view(), name='user_change_password'),
-    path('profile/genpassword/', user_generate_new_password_view, name='user_generate_new_password')
+    path('profile/genpassword/', user_generate_new_password_view, name='user_generate_new_password'),
+
+    # Просмотр пользователей
+    path('all_users/', UserListView.as_view(), name='users_list')
 ]
