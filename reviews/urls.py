e20f7091092c5ebe29917e7c16bd4670ecbbe1f8
Дмитрий Волков <roskey@mail.ru>
2025-04-23 23:05:50 +0300

    1) Добавлен ReviewListView; 2) Добавлен ReviewDeactivatedListView; 3) Созданы новые шаблоны для Reviews; 4) Внесены изменения в текущие шаблоны.

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
 from reviews.apps import ReviewsConfig
+from reviews.views import (ReviewListView, ReviewDeactivatedListView, ReviewCreateView, ReviewDetailView,
+                           ReviewUpdateView, ReviewDeleteView)
 
 app_name = ReviewsConfig.name
 
 urlpatterns = [
-
+    path('', ReviewListView.as_view(), name='reviews_list'),
+    path('deactivated/', ReviewDeactivatedListView.as_view(), name='reviews_deactivated'),
+    path('review/create/', ReviewCreateView.as_view(), name='reviews_deactivated'),
+    path('review/create/', ReviewCreateView.as_view(), name='reviews_deactivated'),
+    path('review/create/', ReviewCreateView.as_view(), name='reviews_deactivated'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.http import HttpResponseForbidden
+from django.shortcuts import reverse, get_object_or_404, redirect
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.core.exceptions import PermissionDenied
 
-# Create your views here.
+from redis.commands.search.querystring import querystring
+
+from reviews.models import Review
+from reviews.forms import ReviewForm
+from users.models import User
+
+
+class ReviewListView(ListView):
+    model = Review
+    template_name = 'reviews/reviews.html'
+    extra_context = {
+        'title': 'Все отзывы'
+    }
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(sign_of_review=True)
+        return queryset
+
+
+class ReviewDeactivatedListView(ListView):
+    model = Review
+    template_name = 'reviews/reviews.html'
+    extra_context = {
+        'title': 'Не активные отзывы'
+    }
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(sign_of_review=False)
+        return queryset
+
+
+class ReviewCreateView(CreateView):
+    ...
+
+
+class ReviewDetailView(DetailView):
+    ...
+
+class ReviewUpdateView(UpdateView):
+    ...
+
+class ReviewDeleteView(DeleteView):
+    ...
